4b69a2393b4763d64228b375e25aa0da52a5b58a
Marcelo Adrian Oliveto <marcelo.oliveto@gmail.com>
2024-03-27 00:15:14 -0300

    login & profile

--- src/config/pasport.config.js
+++ src/config/pasport.config.js
@@ -1,100 +1,44 @@
 const passport = require("passport");
-const local = require("passport-local");
 const jwt = require("passport-jwt");
-const { userModel } = require("../model/user.model");
-const { createHashValue, isValidPasswd } = require("../utils/encrypt");
-const { SECRET_JWT } = require("../utils/jwt");
-const ROLES = require("../constants/role.constants");
-
-const localStrategy = local.Strategy;
 const JWTStrategy = jwt.Strategy;
 const ExtractJWT = jwt.ExtractJwt;
+const { userModel } = require("../model/user.model");
+const { SECRET_JWT } = require("../utils/jwt");
+
+const { createHashValue, isValidPasswd } = require("../utils/encrypt");
+const ROLES = require("../constants/role.constants");
+
+// const local = require("passport-local");
+// const localStrategy = local.Strategy;
+
+function extractJwtFromCookie(req) {
+  const cookies = req.cookies;
+  if (!cookies || !cookies.token) {
+    return null;
+  }
+  return cookies.token;
+}
 
 const initializePassport = () => {
-  /*
+
   passport.use(
-    "register",
-    new localStrategy(
+    'current',
+    new JWTStrategy(
       {
-        passReqToCallback: true,
-        usernameField: "email",
+        jwtFromRequest: extractJwtFromCookie,
+        secretOrKey: SECRET_JWT,
       },
-      async (req, username, password, done) => {
-        console.log(
-          "🚀 ~ file: passport.config.js:17 ~ username: REGISTER STRATEGY",
-          username
-        );
-
-        const { first_name, last_name, email, age } = req.body;
-
-        try {
-          let user = await userModel.findOne({ email });
-          console.log("🚀 ~ file: passport.config.js:19 ~ user:", user);
-          if (user) {
-            // el usuario existe
-            return done(null, false);
-          }
-          const pswHashed = await createHashValue(password);
-
-          const addUser = {
-            first_name,
-            last_name,
-            email,
-            age,
-            password: pswHashed,
-          };
-
-          const newUser = await userModel.create(addUser); // promesa
-
-          if (!newUser) {
-            return res
-              .status(500)
-              .json({ message: `we have some issues register this user` });
-          }
-
-          return res.redirect("/login");
-
-        } catch (error) {
-          return done(`error getting user ${error}`);
+      async (payload, done) => {
+        //console.log("jwtPayload:", payload);
+        const user = payload.user;
+        console.log(user);
+        if (!user) {
+          return done(null, false);
         }
+        return done(null, user);
       }
     )
   );
-
-  passport.use(
-    "login",
-    new localStrategy(
-      {
-        usernameField: "email",
-      },
-      async (username, password, done) => {
-        try {
-          console.log("***LOGIN STRATEGY***");
-          const user = userModel.findOne({ email: username });
-
-          if (!user) {
-            console.log('User does not exist in DB');
-            return done(null, false);
-          }
-
-          console.log(password);
-          console.log(user.password);
-
-          if (!isValidPasswd(password, user.password)) {
-            console.log('User password it not the same in DB');
-            return done(null, false);
-          }
-
-          return done(null, user);
-        } catch (error) {
-          console.log("error:", error);
-
-          return done(error);
-        }
-      }
-    )
-  );
-  */
 
   passport.use(
     "jwt",
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -1,33 +1,71 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const { isValidPasswd } = require("../utils/encrypt");
+const { userModel } = require("../model/user.model");
+const { generateJWT } = require("../utils/jwt");
 
 router.get("/", (req, res) => {
   res.render("index", {})
 })
 
-// Ruta de registro
-router.get('/register', (req, res) => {
-  res.render('register');
-});
-
-// Ruta de login
 router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// Ruta de registro de usuario
-// router.post('/register', passport.authenticate('register', {
-//   successRedirect: '/login',
-//   failureRedirect: '/register',
-//   failureFlash: true
-// }));
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+  const findUser = await userModel.findOne({ email });
+  //console.log("findUser:", findUser);
 
-// // Ruta de login de usuario
-// router.post('/login', passport.authenticate('jwt', {
-//   successRedirect: '/profile',
-//   failureRedirect: '/login',
-//   failureFlash: true
-// }));
+  if (!findUser) {
+    return res
+      .status(401)
+      .json({ message: `este usuario no esta registrado` });
+  }
+
+  const isValidComparePsw = await isValidPasswd(password, findUser.password);
+  if (!isValidComparePsw) {
+    return res.status(401).json({ message: `credenciales invalidas` });
+  }
+
+  const {
+    first_name,
+    last_name,
+    email: emailDb,
+    age,
+    role,
+    carts,
+  } = findUser;
+
+  const token = await generateJWT({
+    first_name,
+    last_name,
+    email: emailDb,
+    age,
+    role,
+    carts,
+    id: findUser._id,
+  });
+
+  res.cookie('token', token, { httpOnly: true });
+  //res.json({ message: 'Login exitoso' });
+  res.redirect('profile');
+});
+
+const authenticate = passport.authenticate('current', { session: false });
+router.get('/profile', authenticate, async (req, res) => {
+  const user = req.user;
+  res.render('profile', { user: user });
+});
+
+router.get('/logout', async (req, res) => {
+  res.clearCookie('token');
+  res.redirect('/login');
+});
+
+router.get('/register', (req, res) => {
+  res.render('register');
+});
 
 module.exports = router;
